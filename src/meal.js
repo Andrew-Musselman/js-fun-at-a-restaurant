0c5636494bf070df8e0087b2e1cde5f55cf48c89
Andrew Musselman <andrew.mussels@gmail.com>
2021-12-03 18:52:07 -0700

    Finish meals.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  }
+  return menuItem;
+}
+
+function addIngredients(ingredient, ingredients) {
+  if (!ingredients.includes(ingredient)) {
+    ingredients.push(ingredient);
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(initialPrice) {
+  return initialPrice * .9
+}
+
+function createRecipe (title, ingredients, type) {
+
+  recipe = {
+    title:  `${title}`,
+    ingredients: ingredients,
+    type: type,
+  }
+   return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
